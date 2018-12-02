a76ed55043a8e8bf512932e86aa851de3ff89b93
professorxin <1252778220@qq.com>
2018-12-02 17:24:16 +0800

    改成maven

--- src/cn/lzx/core/ServerMain.java
+++
@@ -1,9 +0,0 @@
-package cn.lzx.core;
-
-public class ServerMain {
-
-    public static void main(String args[]) throws Exception {
-        ChatServer server=new ChatServer();
-        server.run(8888);
-    }
-}
